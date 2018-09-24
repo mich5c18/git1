27b6ead7b6f8eca9617de65819aeea142e98eb71
michaelserupmj <michaelserupmj@gmail.com>
2018-09-24 13:09:09 +0200

    Helper class tilføjet

--- git1/Program.cs
+++ git1/Program.cs
@@ -10,12 +10,9 @@ namespace git1
     {
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
-
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+            Console.WriteLine("Hello git1");
+            Console.WriteLine("ny ændring");
+            Console.ReadLine();
         }
     }
 }
